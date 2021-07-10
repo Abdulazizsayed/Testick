390fdbff1915d8a455455ff8609d3d85fdaf59b6
abotalebkhaled <36378884+abotalebkhaled@users.noreply.github.com>
2021-07-10 20:26:51 +0200

    Added integrationController , courseController , subjectController, userController and Gmail model
    Now multiple websites can combine their information inside our website automatically. Using the integration API and his form.

---
+++ app/Http/Controllers/integrationController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+use PhpOffice\PhpSpreadsheet;
+use Illuminate\Http\Request;
+use App\Http\Controllers\userController;
+use App\User;
+use App\Http\Controllers\courseController;
+
+class integrationController extends Controller
+{
+    public function integrate(Request $request)
+    {
+        $uploaded_file = $request->file->store('public/uploads'); // default , will upload the file and store it at storage/app/public
+        $path = $request->file->getRealPath();
+        $reader= \PhpOffice\PhpSpreadsheet\IOFactory::createReaderForFile($path);
+        $excel_Obj = $reader->load($path);
+        $worksheet=$excel_Obj->getActiveSheet();
+        $lastRow = $worksheet->getHighestRow();
+        $highestColumn = $worksheet->getHighestDataColumn();
+
+        $Uni = array();
+        $Course = array();
+        $users = array();
+        for($row = 1 ; $row <= $lastRow ; $row++) 
+        {
+            $userObj = new User();
+            for($col='A';$col<=$highestColumn ;$col++)
+            {
+                $cellValue = $worksheet->getCell($col . $row)->getValue();
+                if($cellValue != null)
+                {
+                    if($row == 1) // Univirsty infomration
+                    {
+                        array_push($Uni , $cellValue); 
+                    }
+                    else if ($row == 2 ) // Course infromation
+                    {
+                        array_push($Course , $cellValue);
+                    }
+                    else{ // Users information
+                        if($col == 'A')
+                        {
+                            $userObj['name'] = $cellValue;
+                        }
+                        if($col == 'B')
+                        {
+                            $userObj['email'] = $cellValue;
+                        }
+                        if($col == 'C')
+                        {
+                            $role = 0;
+                            if($cellValue == 'doctor')
+                            {
+                                $role = 1;
+                            }
+                            $userObj['role'] = $role;
+                        }
+                    } 
+                }  
+            }
+            if($row > 2){
+                array_push($users , $userObj);
+            }
+        }
+        $courseObj = new courseController();
+        $courseObj->create($Course);
+        $user = new userController();
+        for($i = 0 ; $i < count($users) ; $i++)
+        {
+            $user->create($users[$i]);
+        }
+        
+
+    }
+}
