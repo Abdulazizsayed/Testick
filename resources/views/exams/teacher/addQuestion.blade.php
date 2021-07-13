92c569d58adfe2dc5d6232e6fcc75001372c8547
OsamaElneshwy <osamatarek920@gmail.com>
2021-07-13 14:22:10 +0200

    add question to exam

--- resources/views/exams/teacher/addQuestion.blade.php
+++ resources/views/exams/teacher/addQuestion.blade.php
@@ -6,105 +6,68 @@ use App\QuestionBank;
 @section('content')
 <div class="container add-question">
     <form action="{{--asset('exams/addQuestion/' . $exam->id)--}}" enctype="multipart/form-data" method="POST">
-    @csrf
-    @method('POST')
+        @csrf
+        @method('POST')
 
-    <h2 class="title">Add Question to {{$exam->title}}<h2>
-    <div class="row">
+        <h2 class="title">Add Question to {{$exam->title}}<h2>
+        <div class="row">
             <div class="col-8">
                 <div class="d-flex">
-                    <div style="float:left;width:50%;margin-right: 10px;">
-                        <select class="form-control" name="parent" id="parent" style="background-color: #1A034A;color: white" >
-                            <option value="null" disabled selected >Question Parent</option>
-                            @foreach( $exam->questions as $question)
-                            {
-                                <option value="{{$question->id}}">{{$question->content}}</option>
-                            }
+                    <div style="float:right;width:40%;margin-left: 10px;">
+                        <select class="form-control" name="question bank" id="question bank" required style="background-color: #1A034A;color: white">
+                            <option value="" disabled selected >Question bank</option>
+                            @foreach( Auth::user()->questionBanks as $questionBank)
+                            <option value="{{$questionBank->id}}">{{$questionBank->title}}</option>
                             @endforeach
                         </select>
-                    </div>
-                    <div>
-                        <input id="chapter" name="chapter" type="text" class="form-control @error('chapter') is-invalid @enderror" chapter="chapter" value="{{ old('chapter') }}" required autocomplete="chapter" autofocus placeholder="Chapter" style="background-color: #1A034A;color: white">
-                        @error('chapter')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                        <div style="float:right;width:20%;margin-left: 10px;">
-                            <select class="form-control type" name="type" id="type" required style="background-color: #1A034A;color: white">
-                                <option value="" disabled selected >Type</option>
-                                <option value="Parent">Parent</option>
-                                <option value="T/F">T/F</option>
-                                <option value="SSMCQ">SSMCQ</option>
-                                <option value="MSMCQ">MSMCQ</option>
-                                <option value="Text Check">Text Check</option>
-                                <option value="Eassay">Eassay</option>
-                            </select>
-                        </div>
-                        <div style="float:right;width:30%;margin-left: 10px;">
-                            <select class="form-control" name="difficulty" id="difficulty" required style="background-color: #1A034A;color: white">
-                                <option value="" disabled selected >Difficulty</option>
-                                <option value="Easy">Easy</option>
-                                <option value="Med">Med</option>
-                                <option value="Hard">Hard</option>
-                            </select>
-                        </div>
-                        <div style="float:right;width:40%;margin-left: 10px;">
-                            <select class="form-control" name="question bank" id="question bank" required style="background-color: #1A034A;color: white">
-                                <option value="" disabled selected >Question bank</option>
-                                @foreach( Auth::user()->questionBanks as $questionBank)
-                                    <option value="{{$questionBank->id}}">{{$questionBank->title}}</option>
-                                @endforeach
-                            </select>
-                        </div>
                     </div>
                 </div>
             </div>
             <br>
 
-        <div class="row">
-            <div class="col-8">
-                <div class="form-group">
-                    <label for="content">Content</label>
-                    <textarea class="form-control @error('Qcontent') is-invalid @enderror" id="content" name="Qcontent" required autocomplete="Qcontent" autofocus>{{ old('Qcontent') }}</textarea>
-                    @error('Qcontent')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+            <div class="row">
+                <div class="col-md-4">
+                    <span class='label'>Filter</span>
+                    <select class="filter-by" name="filter-by">
+                        <option value="1">Type</option>
+                        <option value="3">Question Content</option>
+                    </select>
+                </div>
+                <div class="col-md-8">
+                    <span class='label'>Search by <span class="selected-filter">Title</span></span>
+                    <form id="search-form" autocomplete="off">
+                    @csrf
+                        <input class="search-filter-input" type="text" name="search_input" placeholder="Enter exam Title">
+                        <input class='filter-value' name='filter_value' type="text" value="title" hidden>
+                    </form>
                 </div>
             </div>
-        </div>
 
-        <div class="answers">
-            <div class="row answer">
-                <div class="col-8">
-                    <div>
-                        <div>
-                            <label for="answer1">Answer 1</label>
-                        </div>
-                        <div>
-                            <input id="answer1" name="answer1" type="text" class="form-control @error('answer1') is-invalid @enderror" answer1="answer1" value="{{ old('answer1') }}" required autocomplete="answer1" autofocus>
-                            @error('answer1')
-                            <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <div class="mt-3">
-                            <input class="form-check-input form-control" type="checkbox" id="ch1" name="ch1" value="1">
-                            <label class="form-check-label" for="ch1">
-                                Correct?
-                            </label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="mt-3">
+            <table class="table mt-5">
+                <thead class="thead-blue">
+                    <tr>
+                        <th scope="col">Content</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Difficulty</th>
+                        <th scope="col">Chapter</th>
+                        <th scope="col">Add to Exam</th>
+                    </tr>
+                </thead>
+                <tbody class="exams-holder">
+                    @foreach(QuestionBank::find(2)->questions as $question)
+                    <tr>
+                        <td>{{$question->content}}</td>
+                        <td>{{$question->type}}</td>
+                        <td>{{$question->difficulty}}</td>
+                        <td>{{$question->chapter}}</td>
+                        <td>
+                            <input  type="checkbox" id="ch.{{$question->id}}" name="ch.{{$question->id}}" value="{{$question->id}}"> 
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
             <button type="submit" class="btn btn-success">ADD</button>
-            <div class="btn add add-answer" title="Add new question">Add answer <i class="fa fa-plus fa-lg"></i></div>
         </div>
     </from>
 </div>
