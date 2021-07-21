b63daf543c63db3fbe86676cd5b34bf1e9b48489
AhmedAyman16899 <36383074+AhmedAyman16899@users.noreply.github.com>
2021-07-22 01:54:26 +0200

    add home view

--- resources/views/course/announcementlog.Blade.php
+++ resources/views/course/announcementlog.Blade.php
@@ -6,20 +6,39 @@ use App\User;
 @section('content')
 <div class="container courses">
 
-    @foreach(auth()->user()->announcements as $announcement)
+
+        <div style="background-color: #0A125A;border-radius: 25px;line-height: 20px; word-wrap: break-word;margin-top: 15px">
+            <br>
+            <h1 style="margin-left: 3%;margin-right: 5%"><b>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb</b></h1>
+
+                        <h2 style="margin-left: 6%;margin-top: 3%;margin-right: 5%" ><b>eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</b></h2>
+                        <h5 style="float: right;margin-right: 3%">aaassssaaaaassssssssssssssaaaaaaaaaaaaaa</h5>
+
+                        <br> <br> <br>
+                       <p style="float: right;margin-right: 3%; ">cccssssaaaaaassssssssssssccccccccccccccccccc</p>
+            <br> <br> <br>
+                       <h5 style="margin-left: 9%;margin-right: 5%">ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</h5>
+            <br>
+
+        </div>
+
+    <div style="background-color: #0A125A;border-radius: 25px;line-height: 20px; word-wrap: break-word;margin-top: 15px">
         <div style="background-color: #0A125A;border-radius: 25px;line-height: 20px; word-wrap: break-word;">
             <div>
-                <div style="float: right"><h5 style="margin-right: 170px;margin-top: 50px">{{User::find($announcement->publisher_id)->name}}</h5></div>
-                    <br>
-                        <div><h2 style=" margin-left: 60px;margin-top: 20px"><b>{{$announcement->title}}</b></h2></div>
-                        <p style="margin-left: 800px;color: #DAB40B">{{$announcement->created_at}}</p>
-                        <h5 style="margin-left: 20px;margin-right: 20px ">{{$announcement->content}}</h5>
-                    <br>
-                </div>
+                <div style="float: right"><h5 style="margin-right: 170px;margin-top: 50px">aaa</h5></div>
+                <br>
+                <div><h2 style=" margin-left: 60px;margin-top: 20px"><b>bbb</b></h2></div>
+                <p style="margin-left: 800px;color: #DAB40B">ccc</p>
+                <h5 style="margin-left: 20px;margin-right: 20px ">ddd</h5>
+                <br>
             </div>
         </div>
-        <br>
-    @endforeach
+    </div>
+
+
+</div>
+
+
 
 </div>
 @endsection
